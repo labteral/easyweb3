7a19f875db6812bd41663a314cf6a6c32bebfe9c
Rodrigo <dev@brunneis.com>
2020-07-14 15:25:58 +0200

    Update install_requires

--- easyweb3/__init__.py
+++ easyweb3/__init__.py
@@ -4,7 +4,7 @@
 from .easyweb3 import EasyWeb3
 import logging
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 logging.getLogger().setLevel(logging.INFO)
 logging.basicConfig(format='%(asctime)-15s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
