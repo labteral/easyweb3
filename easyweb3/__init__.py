03e28b8e2328abb3cc8384fa68150facf3f78b02
brunneis <dev@brunneis.com>
2023-08-21 08:15:37 +0000

    Remove logging config

--- easyweb3/__init__.py
+++ easyweb3/__init__.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .easyweb3 import *
-import logging
+from .easyweb3 import *  # noqa: F401, F403
 
-__version__ = '1.215.0'
-
-logging.getLogger().setLevel(logging.INFO)
-logging.basicConfig(format='%(asctime)-15s [%(levelname)s] %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
-logging.info(f'EasyWeb3 v{__version__}')
+__version__ = '1.2308.0'
